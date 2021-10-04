ba950a0ab11be643b6b98f75bc39b27d249ff402
Tobias Kattanek <tkattanek00@gmail.com>
2021-10-04 10:36:27 +0200

    Init
    Init Project, add raylib, simple hello world

---
+++ TicTacToe/TicTacToe/Program.cs
@@ -0,0 +1,25 @@
+﻿using Raylib_cs;
+using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(800, 480, "Hello World");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+                Raylib.DrawText("Hello, world!", 12, 12, 20, Color.BLACK);
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
