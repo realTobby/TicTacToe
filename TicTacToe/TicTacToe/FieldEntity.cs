1ccad9c6b85a9e6089514dc5887e371e1ee32b6d
Tobias Kattanek <tkattanek00@gmail.com>
2021-10-04 11:07:24 +0200

    Game
    Playboard init, select, unselect and click behaviour

---
+++ TicTacToe/TicTacToe/FieldEntity.cs
@@ -0,0 +1,79 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    public class FieldEntity
+    {
+        public int POSX { get; set; }
+        public int POSY { get; set; }
+
+        public int WIDTH { get; set; }
+        public int HEIGHT { get; set; }
+        public Color COLOR { get; set; }
+        public bool ISSELECTED { get; set; } = false;
+        public bool ISCLICKED { get; set; } = false;
+        public Color SELECTED_COLOR = Color.YELLOW;
+        public Color CLICKED_COLOR = Color.RED;
+        public Color NORMAL_COLOR = Color.BLACK;
+
+        public FieldEntity(int x, int y, int w, int h)
+        {
+            POSX = x;
+            POSY = y;
+            WIDTH = w;
+            HEIGHT = h;
+            COLOR = Color.BLACK;
+        }
+
+        public void Select()
+        {
+            ISSELECTED = true;
+            COLOR = SELECTED_COLOR;
+        }
+
+        public void Unselect()
+        {
+            ISSELECTED = false;
+            COLOR = NORMAL_COLOR;
+        }
+
+        public void CLICK()
+        {
+            ISCLICKED = !ISCLICKED;
+            if(ISCLICKED == true)
+            {
+                COLOR = CLICKED_COLOR;
+                ISSELECTED = false;
+            }
+
+            if(ISCLICKED == false)
+            {
+                COLOR = NORMAL_COLOR;
+                ISCLICKED = false;
+                ISSELECTED = false;
+            }
+        }
+
+
+        public bool IsMouseInside(int mx, int my)
+        {
+            if(mx > POSX && mx < POSX + WIDTH && my > POSY && my < POSY+HEIGHT)
+            {
+                ISSELECTED = true;
+                return true;
+            }
+
+            ISSELECTED = false;
+            return false;
+        }
+
+        public void Draw()
+        {
+            Raylib.DrawRectangle(POSX, POSY, WIDTH, HEIGHT, COLOR);
+        }
+
+    }
+}
--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -1,25 +1,94 @@
 ﻿using Raylib_cs;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TicTacToe
 {
     class Program
     {
+        static List<FieldEntity> playboardEntities = new List<FieldEntity>();
+
+        static void InitPlayboard()
+        {
+            playboardEntities.Add(new FieldEntity(0, 0, 50, 50));
+            playboardEntities.Add(new FieldEntity(100, 0, 50, 50));
+            playboardEntities.Add(new FieldEntity(200, 0, 50, 50));
+
+            playboardEntities.Add(new FieldEntity(0, 100, 50, 50));
+            playboardEntities.Add(new FieldEntity(100, 100, 50, 50));
+            playboardEntities.Add(new FieldEntity(200, 100, 50, 50));
+
+            playboardEntities.Add(new FieldEntity(0, 200, 50, 50));
+            playboardEntities.Add(new FieldEntity(100, 200, 50, 50));
+            playboardEntities.Add(new FieldEntity(200, 200, 50, 50));
+
+
+        }
+
+        static void CheckMouseOverField()
+        {
+            var mousePos = Raylib.GetMousePosition();
+
+            foreach(var fe in playboardEntities.ToList())
+            {
+                if(fe.IsMouseInside(Convert.ToInt32(mousePos.X), Convert.ToInt32(mousePos.Y)))
+                {
+                    if(fe.ISCLICKED == false)
+                        fe.Select();
+                }
+                else
+                {
+                    if(fe.ISCLICKED == false)
+                        fe.Unselect();
+                }
+            }
+
+        }
+
         static void Main(string[] args)
         {
-            Raylib.InitWindow(800, 480, "Hello World");
+            Raylib.InitWindow(300, 300, "Tic-Tac-Toe");
+
+            InitPlayboard();
 
             while (!Raylib.WindowShouldClose())
             {
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.WHITE);
 
-                Raylib.DrawText("Hello, world!", 12, 12, 20, Color.BLACK);
+                DebugShowMousePosition();
+                DrawPlayboard();
+                CheckMouseOverField();
 
+                if(Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON))
+                {
+                    var clickedField = playboardEntities.Where(item => item.ISSELECTED == true).FirstOrDefault();
+                    if(clickedField != null)
+                    {
+                        clickedField.CLICK();
+                    }
+                }
+                
                 Raylib.EndDrawing();
             }
 
             Raylib.CloseWindow();
+        }
+
+        private static void DebugShowMousePosition()
+        {
+            var pos = Raylib.GetMousePosition();
+
+            Raylib.DrawText(pos.X + " / " + pos.Y, 50, 50, 24, Color.BLUE);
+        }
+
+        private static void DrawPlayboard()
+        {
+            foreach(var fe in playboardEntities)
+            {
+                fe.Draw();
+            }
         }
     }
 }
